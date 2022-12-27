2c4a87bccbde89ff8cef500d2b24d211a440a9dd
Umar Asif <umarasif737@gmail.com>
2022-12-28 00:32:55 +0500

    Partner schema made

--- src/api/partner/controllers/partner.js
+++ src/api/partner/controllers/partner.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- *  review controller
+ * partner controller
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::review.review');
+module.exports = createCoreController('api::partner.partner');
--- src/api/partner/services/partner.js
+++ src/api/partner/services/partner.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * review service.
+ * partner service
  */
 
 const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::review.review');
+module.exports = createCoreService('api::partner.partner');
